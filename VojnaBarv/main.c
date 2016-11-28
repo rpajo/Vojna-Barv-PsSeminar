3d768c00e3343f3f27d0e0a3ea98a3fdebfa0e9c
jakobmerljak <merljak.jakob@gmail.com>
2016-11-28 12:21:53 +0100

    Comment SDL and Windows code

--- VojnaBarv/main.c
+++ VojnaBarv/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL.h>
+//#include <SDL.h>
+//#include "render.h"
 #include "grid.h"
-#include "render.h"
 #include "file.h"
-#include <Windows.h>
+//#include <Windows.h>
 
-#define WINDOW_NAME		"Vojna Barv"
-#define DELAY			((unsigned int) 100)
+//#define WINDOW_NAME		"Vojna Barv"
+//#define DELAY			((unsigned int) 100)
 #define ITERATIONS		500
 #define WINDOW			2
 #define FILE_NAME		"grid1.txt"
@@ -19,18 +19,18 @@ int main(int argc, char **argv) {
 	Grid *grid = config->initialGrid;
 	Grid *tempGrid = createGrid(grid->height, grid->width);
 	if (tempGrid == NULL) printf("main error: cannot allocate space for temporary grid.\n");
-	Renderer *renderer = initRenderer(WINDOW_NAME, grid, config->cellSize);
+	/*Renderer *renderer = initRenderer(WINDOW_NAME, grid, config->cellSize);
 	if (renderer == NULL || createTexturesFromColors(config, renderer) != 0 || tempGrid == NULL) {
 		destroyRenderer(grid, renderer);
 		destroyGrid(grid);
 		destroyGrid(tempGrid);
 		destroyGridFile(config);
 		return 1;
-	}
+	}*/
 	config->initialGrid = NULL;
 	destroyGridFile(config);
 
-	LARGE_INTEGER frequency;        // ticks per second
+	/*LARGE_INTEGER frequency;        // ticks per second
 	LARGE_INTEGER t1, t2;           // ticks
 	double elapsedTime;
 
@@ -38,17 +38,17 @@ int main(int argc, char **argv) {
 	QueryPerformanceFrequency(&frequency);
 
 	/////// TIMER START ///////
-	QueryPerformanceCounter(&t1);
+	QueryPerformanceCounter(&t1);*/
 
 	unsigned int iterations = ITERATIONS;
 	
-	SDL_Event sdlEvent;
-	int run = 1;
-	while (run && iterations) {
+	/*SDL_Event sdlEvent;
+	int run = 1;*/
+	while (/*run && */iterations) {
 		
 		processGrid(grid, tempGrid, WINDOW);
 		
-		// render grid and display rendered content
+		/*// render grid and display rendered content
 		renderGrid(grid, renderer);
 		SDL_RenderPresent(renderer->SDLrenderer);
 
@@ -57,17 +57,17 @@ int main(int argc, char **argv) {
 			if (sdlEvent.type == SDL_QUIT) // window's X button
 				run = 0;
 		// delay in miliseconds
-		SDL_Delay(DELAY);
+		SDL_Delay(DELAY);*/
 		
 		iterations--;
 	}
 
-	// end time
+	/*// end time
 	QueryPerformanceCounter(&t2);
 	elapsedTime = (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
 	printf("%f ms\n", elapsedTime);
 
-	destroyRenderer(grid, renderer);
+	destroyRenderer(grid, renderer);*/
 	//destroyGrid(grid);
 	//destroyGrid(tempGrid);
 	return 0;
